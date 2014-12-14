0d0851cacc04f4f19bd3b131976d1db7518b1dec
Alex Zai <azai91@gmail.com>
2014-12-13 18:31:54 -0800

    (feat) hoists variables inside scope

--- classes/Variables.js
+++ classes/Variables.js
@@ -1,31 +1,76 @@
 'use strict';
 
+var utils = require('../utils');
+
 var variableHandler = {
 
 
 
 };
 
-//hoists non declared variables
+
+/**
+ * [hoistVariables description]
+ * @param  {[type]} bufferArray [description]
+ * @return {[type]}             [description]
+ */
 var hoistVariables = function (bufferArray) {
-    var previousScope = 0,
-        undeclaredVariableList = [],
-        previousVariable,
-        variableName;
+  var previousScope = 0,
 
-    for (var i = 0; i < bufferArray.length; i++) {
-      if (hasVariableName(bufferArray[i])) {
-        variableName = hasVariableName(bufferArray[i]);
+      //contains elements with [line number, bracket count, array of variables]
+      undeclaredVariableList = [],
+      previousScopeLine = [],
+      previousVariable,
+      variableName;
 
-        if (!isVariableDeclared(bufferArray[i], variableName)) {
-          undeclaredVariableList.push('var ' + variableName + ';');
+  undeclaredVariableList.push([ 0, 0, [] ]);
+
+  for (var i = 0; i < bufferArray.length; i++) {
+
+    //checks if function is in line
+    if (hasFunction(bufferArray[i])) {
+      undeclaredVariableList.push([ i, 0, [] ]);
+    }
+    // console.log(undeclaredVariableList);
+    undeclaredVariableList[undeclaredVariableList.length - 1][1] += getCurlyBracketCount(bufferArray[i]);
+
+
+    if (hasVariableName(bufferArray[i])) {
+      variableName = hasVariableName(bufferArray[i]);
+
+      // console.log(variableName);
+
+      if (!isVariableDeclared(bufferArray[i], variableName)) {
+        var space = "";
+        for (var scopeCounter = 0; scopeCounter < undeclaredVariableList.length - 1; scopeCounter++) {
+          space += "  ";
         }
+        undeclaredVariableList[undeclaredVariableList.length - 1][2].push(space + 'var ' + variableName + ';');
       }
     }
+    console.log('one');
+    console.log('bitch', undeclaredVariableList[undeclaredVariableList.length - 1][1]);
+    console.log(undeclaredVariableList[undeclaredVariableList.length - 1]);
+    //end of scope, not first line
+    if (undeclaredVariableList[undeclaredVariableList.length - 1][1] === 0 && undeclaredVariableList.length !== 1) {
+      var variables = undeclaredVariableList.pop();
+      console.log('variables', variables);
+      console.log('hoist');
+      bufferArray = utils.concatArrayInsideArray(variables[2], variables[0] + 1, bufferArray);
+      console.log(bufferArray);
+      i += variables[2].length;
+    }
+  }
 
-    return undeclaredVariableList.concat(bufferArray);
+  return undeclaredVariableList[0][2].concat(bufferArray);
 }
 
+/**
+ * checks to see line has variable by looking for single equal sign
+ *
+ * @param  {String}  bufferLine
+ * @return {Boolean}
+ */
 var hasVariableName = function (bufferLine) {
   var equals = false,
       variableNameReversed = '',
@@ -34,7 +79,6 @@ var hasVariableName = function (bufferLine) {
   // looks for single equals sign. If found then will extract variable name and check if prefaced with 'var'
   if (bufferLine.match(/[^=]\s?=\s[^=]/g)) {
     for (var i = bufferLine.length - 1; i >= 0; i--) {
-
       if (equals && bufferLine[i] !== ' ' && !endOfVariable) {
         variableNameReversed += bufferLine[i];
       }
@@ -49,6 +93,29 @@ var hasVariableName = function (bufferLine) {
 
   return variableNameReversed.split('').reverse().join('');
 }
+
+/**
+ * checks if the line has function in it
+ *
+ * @param  {String}  bufferLine
+ * @return {Boolean}
+ */
+var hasFunction = function (bufferLine) {
+  return bufferLine.match(/function/) ? true : false;
+};
+
+var getCurlyBracketCount = function (bufferLine) {
+  var counter = 0;
+  for (var i = 0; i < bufferLine.length; i++) {
+    if (bufferLine[i] === '{') {
+      counter++;
+    }
+    if (bufferLine[i] === '}') {
+      counter--;
+    }
+  }
+  return counter;
+};
 
 var isVariableDeclared = function (bufferLine, variableName) {
   bufferLine = bufferLine.slice(0, bufferLine.indexOf(variableName));
--- output.js
+++ output.js
@@ -1,13 +1,14 @@
 var test;
-var five;
-var arr = [1,2,3,4];
+test = 5;
 
-for (var _i = 1 ; _i <= 10; _i++) {
-  test.push(_i);
+function() {
+  var test2;
+  test2 = 5;
+  function() {
+    var test3;
+    test3 = 6;
+
+  }
+
+
 }
-
-var small = big.slice(1, 12);
-
-five = 5;
-
-var six = 6;
